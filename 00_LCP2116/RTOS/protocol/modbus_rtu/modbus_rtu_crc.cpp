579b940887f1f6fdf68a28346695b37f44ea3965
R&D <S.zakrz@gmail.com>
2026-07-18 20:10:26 +0200

    Implement Modbus RTU CRC software self-test

--- 00_LCP2116/RTOS/protocol/modbus_rtu/modbus_rtu_crc.cpp
+++ 00_LCP2116/RTOS/protocol/modbus_rtu/modbus_rtu_crc.cpp
@@ -33,3 +33,16 @@ uint16_t modbus_rtu_crc16(const uint8_t* data, size_t length)
 
     return crc;
 }
+
+uint8_t modbus_rtu_crc_self_test(void)
+{
+    static const uint8_t TEST_FRAME[] =
+    {
+        0x01U, 0x03U, 0x00U, 0x00U, 0x00U, 0x01U
+    };
+    static const uint16_t EXPECTED_CRC = 0x0A84U;
+
+    return (modbus_rtu_crc16(TEST_FRAME, sizeof(TEST_FRAME)) == EXPECTED_CRC)
+               ? 1U
+               : 0U;
+}
