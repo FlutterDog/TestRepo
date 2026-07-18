01cf40bca8d1c0115ff008bcd958d7e85ef341c5
R&D <S.zakrz@gmail.com>
2026-07-18 19:28:08 +0200

    Add Modbus RTU CRC interface

---
+++ 00_LCP2116/RTOS/protocol/modbus_rtu/modbus_rtu_crc.hpp
@@ -0,0 +1,21 @@
+﻿/**
+ * @file modbus_rtu_crc.hpp
+ * @brief Расчёт контрольной суммы Modbus RTU CRC-16.
+ */
+
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * @brief Вычисляет стандартную CRC-16 Modbus с полиномом 0xA001.
+ *
+ * В Modbus RTU младший байт результата передаётся первым.
+ *
+ * @param data Указатель на данные.
+ * @param length Количество байтов.
+ *
+ * @return Значение CRC-16.
+ */
+uint16_t modbus_rtu_crc16(const uint8_t* data, size_t length);
