f16828e712ed92635bed22cf7b91134812ee97d2
R&D <S.zakrz@gmail.com>
2026-07-18 19:37:24 +0200

    Add X2X diagnostic interface

---
+++ 00_LCP2116/RTOS/app/diagnostics/x2x_status.hpp
@@ -0,0 +1,24 @@
+﻿/**
+ * @file x2x_status.hpp
+ * @brief USB-диагностика конфигурации и опроса модулей X2X.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+/** @brief Печатает полный отчёт X2X в SerialUSB. */
+void x2x_status_print_report(void);
+
+/**
+ * @brief Обрабатывает сервисную команду X2X.
+ *
+ * Поддерживаются команды:
+ * - x2x, x2x status;
+ * - x2x reload;
+ * - x2x pause, x2x resume;
+ * - x2x ldo SLAVE VALUE.
+ *
+ * @return 1, если команда относится к X2X, иначе 0.
+ */
+uint8_t x2x_status_handle_command(const char* command);
