d14e93c685457134a71774f28131bc43d236684e
R&D <S.zakrz@gmail.com>
2026-07-18 19:36:41 +0200

    Allow X2X service to own UART0

--- 00_LCP2116/RTOS/app/diagnostics/rs485_echo_test.hpp
+++ 00_LCP2116/RTOS/app/diagnostics/rs485_echo_test.hpp
@@ -1,17 +1,25 @@
-﻿
-/**
+﻿/**
  * @file rs485_echo_test.hpp
  * @brief Диагностический echo-test встроенных RS-485 портов.
  */
 
 #pragma once
 
-/**
- * @brief Инициализирует встроенные порты для RS-485 echo-test.
- */
+#include <stdint.h>
+
+/** @brief Инициализирует встроенные порты для RS-485 echo-test. */
 void rs485_echo_test_init(void);
 
-/**
- * @brief Обслуживает RS-485 echo-test.
- */
+/** @brief Обслуживает разрешённые линии RS-485 echo-test. */
 void rs485_echo_test_poll(void);
+
+/**
+ * @brief Разрешает или запрещает echo-test на физической линии X2X.
+ *
+ * X2X-сервис должен запрещать echo до начала Modbus master обмена, чтобы два
+ * обработчика не читали один RX-буфер и не формировали конкурирующие ответы.
+ */
+void rs485_echo_test_set_x2x_enabled(uint8_t enabled);
+
+/** @brief Возвращает 1, если echo-test линии X2X разрешён. */
+uint8_t rs485_echo_test_x2x_enabled(void);
