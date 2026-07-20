5bc927117f8a434eb60f8daeb7175d8dec99bf6e
R&D <S.zakrz@gmail.com>
2026-07-20 17:29:31 +0200

    Document X2X driver entry points

--- 00_LCP2116/RTOS/app/x2x/modules/x2x_module_drivers.hpp
+++ 00_LCP2116/RTOS/app/x2x/modules/x2x_module_drivers.hpp
@@ -1,32 +1,36 @@
 ﻿/**
  * @file x2x_module_drivers.hpp
- * @brief Объявления драйверов штатных модулей X2X.
+ * @brief Публичные entry points штатных драйверов модулей X2X.
+ *
+ * Каждая функция соответствует сигнатуре X2XModulePollFunction и регистрируется
+ * ровно один раз в x2x_catalog.cpp. Драйверы остаются разделёнными по типам,
+ * чтобы аппаратные карты регистров и state machine были видны явно.
  */
 
 #pragma once
 
 #include "../x2x_module.hpp"
 
-/** @brief Выполняет один неблокирующий шаг опроса LDO1118. */
+/** @copydoc X2XModulePollFunction */
 X2XModulePollResult x2x_module_poll_ldo1118(X2XDeviceHeader* device,
-                                            X2XModuleContext& context);
+                                             X2XModuleContext& context);
 
-/** @brief Выполняет один неблокирующий шаг опроса LAI1118. */
+/** @copydoc X2XModulePollFunction */
 X2XModulePollResult x2x_module_poll_lai1118(X2XDeviceHeader* device,
-                                            X2XModuleContext& context);
+                                             X2XModuleContext& context);
 
-/** @brief Выполняет один неблокирующий шаг опроса LDI1118. */
+/** @copydoc X2XModulePollFunction */
 X2XModulePollResult x2x_module_poll_ldi1118(X2XDeviceHeader* device,
-                                            X2XModuleContext& context);
+                                             X2XModuleContext& context);
 
-/** @brief Выполняет один неблокирующий шаг опроса LDI1116. */
+/** @copydoc X2XModulePollFunction */
 X2XModulePollResult x2x_module_poll_ldi1116(X2XDeviceHeader* device,
-                                            X2XModuleContext& context);
+                                             X2XModuleContext& context);
 
-/** @brief Выполняет один неблокирующий шаг опроса LCT1114. */
+/** @copydoc X2XModulePollFunction */
 X2XModulePollResult x2x_module_poll_lct1114(X2XDeviceHeader* device,
-                                            X2XModuleContext& context);
+                                             X2XModuleContext& context);
 
-/** @brief Выполняет один неблокирующий шаг опроса LCT1114 второй версии. */
+/** @copydoc X2XModulePollFunction */
 X2XModulePollResult x2x_module_poll_lct1114_2(X2XDeviceHeader* device,
-                                              X2XModuleContext& context);
+                                               X2XModuleContext& context);
