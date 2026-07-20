7c138e8fd62d24ed5249398004228ef84f8983e1
R&D <S.zakrz@gmail.com>
2026-07-20 16:48:32 +0200

    Bump version for canonical SD contract

--- 00_LCP2116/RTOS/app/version.hpp
+++ 00_LCP2116/RTOS/app/version.hpp
@@ -6,6 +6,6 @@
 #pragma once
 
 #define LCP_DIAGNOSTIC_SOFTWARE_NAME "LCP Basic Diagnostic Firmware"
-#define LCP_DIAGNOSTIC_SOFTWARE_VERSION "0.18.0"
-#define LCP_DIAGNOSTIC_SOFTWARE_STAGE "Stage 18A: Serial config and Modbus TCP"
+#define LCP_DIAGNOSTIC_SOFTWARE_VERSION "0.18.1"
+#define LCP_DIAGNOSTIC_SOFTWARE_STAGE "Stage 18B: Canonical SD configuration"
 #define LCP_DIAGNOSTIC_SOFTWARE_TARGET "ATSAM3X8E"
