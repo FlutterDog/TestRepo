9519a9c3d854c05d1515d8b050e9891bccc911fe
R&D <S.zakrz@gmail.com>
2026-07-20 11:38:31 +0200

    Add FieldSensor diagnostics interface

---
+++ 00_LCP2116/RTOS/app/diagnostics/field_status.hpp
@@ -0,0 +1,18 @@
+﻿/**
+ * @file field_status.hpp
+ * @brief Диагностика демонстрационного FieldSensor на S1..S4.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+/** @brief Печатает состояние всех четырёх FieldSensor master. */
+void field_status_print_report(void);
+
+/**
+ * @brief Обрабатывает команды field, field status, field pause и field resume.
+ *
+ * @return 1, если команда распознана.
+ */
+uint8_t field_status_handle_command(const char* command);
