8bc1b31281a68634fcf46b92226c5a443ab0b641
R&D <S.zakrz@gmail.com>
2026-07-20 17:36:33 +0200

    Bump version for code review and Doxygen stage

--- 00_LCP2116/RTOS/app/version.hpp
+++ 00_LCP2116/RTOS/app/version.hpp
@@ -6,6 +6,6 @@
 #pragma once
 
 #define LCP_DIAGNOSTIC_SOFTWARE_NAME "LCP Basic Diagnostic Firmware"
-#define LCP_DIAGNOSTIC_SOFTWARE_VERSION "0.18.1"
-#define LCP_DIAGNOSTIC_SOFTWARE_STAGE "Stage 18B: Canonical SD configuration"
+#define LCP_DIAGNOSTIC_SOFTWARE_VERSION "0.18.2"
+#define LCP_DIAGNOSTIC_SOFTWARE_STAGE "Stage 18C: Code review and Doxygen"
 #define LCP_DIAGNOSTIC_SOFTWARE_TARGET "ATSAM3X8E"
