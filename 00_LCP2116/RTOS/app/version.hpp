6107bf8bb12484fde42fb4e6feb0e27617bb53f5
R&D <S.zakrz@gmail.com>
2026-07-20 12:48:16 +0200

    Bump firmware for serial config and Modbus TCP

--- 00_LCP2116/RTOS/app/version.hpp
+++ 00_LCP2116/RTOS/app/version.hpp
@@ -1,27 +1,11 @@
-﻿
-/**
+﻿/**
  * @file version.hpp
- * @brief Версия диагностической прошивки LCP.
+ * @brief Версия базовой прошивки LCP.
  */
 
 #pragma once
 
-/**
- * @brief Наименование диагностической прошивки.
- */
 #define LCP_DIAGNOSTIC_SOFTWARE_NAME "LCP Basic Diagnostic Firmware"
-
-/**
- * @brief Версия диагностической прошивки.
- */
-#define LCP_DIAGNOSTIC_SOFTWARE_VERSION "0.17.0"
-
-/**
- * @brief Идентификатор текущего этапа диагностической прошивки.
- */
-#define LCP_DIAGNOSTIC_SOFTWARE_STAGE "Stage 17A"
-
-/**
- * @brief Целевая аппаратная платформа.
- */
+#define LCP_DIAGNOSTIC_SOFTWARE_VERSION "0.18.0"
+#define LCP_DIAGNOSTIC_SOFTWARE_STAGE "Stage 18A"
 #define LCP_DIAGNOSTIC_SOFTWARE_TARGET "ATSAM3X8E"
