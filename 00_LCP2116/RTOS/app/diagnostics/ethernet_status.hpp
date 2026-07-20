da1cd899b05387131ad294f269099a116df8a1d1
R&D <S.zakrz@gmail.com>
2026-07-20 12:39:25 +0200

    Add Ethernet Modbus diagnostics API

---
+++ 00_LCP2116/RTOS/app/diagnostics/ethernet_status.hpp
@@ -0,0 +1,14 @@
+﻿/**
+ * @file ethernet_status.hpp
+ * @brief Service console для двух Modbus TCP server W5500.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+/** @brief Печатает состояние обоих Ethernet-интерфейсов. */
+void ethernet_status_print_report(void);
+
+/** @brief Обрабатывает команды eth и eth reload. */
+uint8_t ethernet_status_handle_command(const char* command);
