01b2bd748779d4bed3bf0aa9450835976ed1bd9c
R&D <S.zakrz@gmail.com>
2026-07-22 06:49:06 +0200

    Add CRC32 helper for configuration storage

---
+++ 00_LCP2116/RTOS/libs/lcp_crc32/lcp_crc32.hpp
@@ -0,0 +1,17 @@
+﻿/**
+ * @file lcp_crc32.hpp
+ * @brief Небольшой CRC-32/ISO-HDLC без динамической памяти.
+ */
+
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * @brief Вычисляет CRC-32/ISO-HDLC (polynomial 0xEDB88320).
+ * @param[in] data Буфер; null допустим только при length=0.
+ * @param[in] length Количество байтов.
+ * @return CRC с initial/final XOR 0xFFFFFFFF.
+ */
+uint32_t lcp_crc32(const void* data, size_t length);
